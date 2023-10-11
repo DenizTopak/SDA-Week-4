fec57eb260c1b571e3be14c6ce521ca2be18dc35
BramW03 <146813905+BramW03@users.noreply.github.com>
2023-10-11 09:42:57 +0200

    Added final project python files and structure.
    All other code can be implemented in object oriented format later in the classes in this folder.
    This will be the folder you download to run the entire code from VScode.

---
+++ PythonCode/FinalPrjCode/Main.py
@@ -0,0 +1,14 @@
+import sys
+import DoBot as Dbt             # add DoBot class code
+from CameraClasses import *     # add camera and UI class code
+
+
+def main():
+    print("yeet")
+    
+    botController = DoBot()
+
+
+# run the main method
+if (__name__ == '__main__'):
+    main()
