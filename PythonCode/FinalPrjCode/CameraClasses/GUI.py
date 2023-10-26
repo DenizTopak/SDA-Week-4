95bd26d6ed25752b0b6f7c93c0a1f04e63dfc47f
MikSzy <146485343+MikSzy@users.noreply.github.com>
2023-10-26 14:10:10 +0200

    Add files via upload
    Changes in the class so it can be initialized before the while loop

--- PythonCode/FinalPrjCode/CameraClasses/GUI.py
+++ PythonCode/FinalPrjCode/CameraClasses/GUI.py
@@ -12,27 +12,75 @@ named_colors = {
     "black": (0, 0, 0)
 }
 
-#Lines for the text (only 3 lines fit)
 line = [10, 25, 40]
 
 class GUI():
-    def __init__(self, window, x1, x2, y1, y2):
-        self.window = window
+    def __init__(self, x1, x2, y1, y2):
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
 
-    #Draw a black square to put the text on
-    def DrawLabelTop(self, bg_color):
+    def DrawLabelTop(self,window, bg_color):
         thickness = cv2.FILLED
 
-        cv2.rectangle(self.window, (0, 0), (300, 50), bg_color, thickness)
-
-    #Put text in, position is arbitrary, however for best effect use (0, line[n])
-    def Text(self, text, pos, bg_color):
+        cv2.rectangle(window, (0, 0), (300, 50), bg_color, thickness)
+    
+    def Text(self, window, text, pos, bg_color):
         font_face = cv2.FONT_HERSHEY_SIMPLEX
         scale = 0.4
         color = bg_color
         
-        cv2.putText(self.window, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
+        cv2.putText(window, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
+    
+    named_colors = {
+        "red": (255, 0, 0),
+        "green": (0, 255, 0),
+        "blue": (0, 0, 255),
+        "yellow": (255, 255, 0),
+        "magenta": (255, 0, 255),
+        "cyan": (0, 255, 255),
+        "white": (255, 255, 255),
+        "black": (0, 0, 0)
+    }
+
+    line = [10, 25, 40]
+
+        
+video = cv2.VideoCapture(0)
+
+
+
+x1, y1, x2, y2 = 115, 135, 385, 475
+
+Ui = GUI(x1, x2, y1, y2)
+
+if (video.isOpened() == False):
+    print('Error opening stream')
+
+#start the stream
+while(video.isOpened()):
+    ret, frame = video.read()
+    cropped_frame = frame[y1:y2, x1:x2]
+    
+
+
+    if ret == True:
+        Ui.DrawLabelTop(cropped_frame, named_colors["black"])
+        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[0]), named_colors['white'])
+        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[1]), named_colors['white'])
+        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[2]), named_colors['white'])
+        cv2.imshow('Window', cropped_frame)
+        
+        k = cv2.waitKey(20)
+        if k == ord('q'):
+             break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()
+
+
+
+
