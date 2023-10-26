f372444cec493180d1000704ea62314625b6b3f4
MikSzy <146485343+MikSzy@users.noreply.github.com>
2023-10-26 14:14:37 +0200

    Add files via upload

--- PythonCode/FinalPrjCode/CameraClasses/GUI.py
+++ PythonCode/FinalPrjCode/CameraClasses/GUI.py
@@ -12,6 +12,7 @@ named_colors = {
     "black": (0, 0, 0)
 }
 
+#List of each line coordinates
 line = [10, 25, 40]
 
 class GUI():
@@ -46,40 +47,6 @@ class GUI():
 
     line = [10, 25, 40]
 
-        
-video = cv2.VideoCapture(0)
-
-
-
-x1, y1, x2, y2 = 115, 135, 385, 475
-
-Ui = GUI(x1, x2, y1, y2)
-
-if (video.isOpened() == False):
-    print('Error opening stream')
-
-#start the stream
-while(video.isOpened()):
-    ret, frame = video.read()
-    cropped_frame = frame[y1:y2, x1:x2]
-    
-
-
-    if ret == True:
-        Ui.DrawLabelTop(cropped_frame, named_colors["black"])
-        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[0]), named_colors['white'])
-        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[1]), named_colors['white'])
-        Ui.Text(cropped_frame, 'Triangle: Red - Coordinates (X, Y)', (0, line[2]), named_colors['white'])
-        cv2.imshow('Window', cropped_frame)
-        
-        k = cv2.waitKey(20)
-        if k == ord('q'):
-             break
-    else:
-        break
-
-video.release()
-cv2.destroyAllWindows()
 
 
 
